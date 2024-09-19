860a3fa2bbfdd4d4885cbf8ca91f1d712d455257
Silvia Santamaria <silviasantabuil@outlook.com>
2024-09-20 00:36:17 +0100

    first commit

---
+++ nea practice.py
@@ -0,0 +1,15 @@
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption('tutorial')
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    pygame.display.update()
+    clock.tick(60)
